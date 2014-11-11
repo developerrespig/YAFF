037956b6c8de2af31d3e860f7d63fc3235cec289
Martin Gypser <developerrespig@googlemail.com>
2014-11-11 09:24:17 +0100

    #6
    - getValuesAction now returns temperature values of the last 24hours for the given device

--- src/YAFF/FHEM/Controller/FHEMServiceController.php
+++ src/YAFF/FHEM/Controller/FHEMServiceController.php
@@ -16,11 +16,14 @@
 
         public function getValuesAction($device)
         {
-            $url = "http://141.56.131.26:8083/fhem?cmd=get%20log_datenbank%20-%20webchart%202014-11-04_00:00:00%202014-11-05_00:00:00%20" . $device . "%20timerange%20TIMESTAMP%20temp_c&XHR=1";
+            $startTimestamp = date('Y-m-d_h:i:s', strtotime ("-24 hours"));
+            $endTimestamp = date('Y-m-d_h:i:s', time());
+
+            $url = "http://141.56.131.26:8083/fhem?cmd=get%20log_datenbank%20-%20webchart%20" . $startTimestamp . "%20" . $endTimestamp . "%20" . $device . "%20timerange%20TIMESTAMP%20temp_c&XHR=1";
 
             $response = $this->app['FHEMService']->createRequest($url);
 
-            return "Test" . $response;
+            return $response;
         }
     }
 ?>
