b77d452472e02cd19ce901e4d213de8cf9ecdfb4
Martin Gypser <developerrespig@googlemail.com>
2014-11-14 17:43:57 +0100

    #6
    - refactored FHEMService and FHEMServiceController
    - implemented function to fetch information for single device
    - added timeout to requests
    - added status code to responses

--- src/YAFF/FHEM/Service/FHEMService.php
+++ src/YAFF/FHEM/Service/FHEMService.php
@@ -5,6 +5,8 @@ namespace YAFF\FHEM\Service;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Response;
 
+use YAFF\Database\Entity\FHEMConfiguration;
+
 /**
  * Description of FHEMService
  *
@@ -13,10 +15,34 @@ use Symfony\Component\HttpFoundation\Response;
 
 class FHEMService
 {
-    private $em = null;
+    private $app = null;
 
     public function __construct(Application $app) {
-        $this->em = $app['orm.em'];
+        $this->app = $app;
+    }
+
+    private function getFHEMConfig() {
+        $em = $this->app['orm.em'];
+        $configs = $em->getRepository("\YAFF\Database\Entity\FHEMConfiguration")->findAll();
+
+        if(count($configs) != 0) {
+            $return = $configs[0];
+        } else {
+            $return = null;
+        }
+
+        return $return;
+    }
+
+    public function getUrl($command) {
+        $config = $this->getFHEMConfig();
+        if($config) {
+            $url = "http://" . $config->getHost() . ":" . $config->getPort() . "/fhem?cmd=" . $command . "&XHR=1";
+        } else {
+            $url = null;
+        }
+
+        return $url;
     }
 
     /**
@@ -31,17 +57,27 @@ class FHEMService
         if ($parameters == null) {
             curl_setopt_array($curl, array(
                 CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_URL => $url
+                CURLOPT_URL => $url,
+                CURLOPT_CONNECTTIMEOUT => 10,
+                CURLOPT_TIMEOUT => 400
             ));
         } else {
             curl_setopt_array($curl, $parameters);
         }
         $response = curl_exec($curl);
+
+        if(curl_errno($curl)) {
+            $statusCode = 500;
+            $response = "Error: " . curl_error($curl);
+        } else {
+            $statusCode = 200;
+        }
+
         curl_close($curl);
 
         return new Response(
             $response,
-            200,
+            $statusCode,
             ['Content-Type' => 'application/json']
         );
     }
